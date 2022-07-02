0065d3787762d8a5959ab8489c7dc03255f3a648
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-02 18:03:13 +0600

    Добавил стили

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,6 +3,6 @@ from django.forms import widgets
 
 
 class GuestbookForm(forms.Form):
-    author = forms.CharField(max_length=20, required=True, label='Имя автора')
-    email = forms.EmailField(empty_value=True, required=True, label='Email автора')
-    entry = forms.CharField(max_length=2000, required=True, label='Entry', widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
+    author = forms.CharField(max_length=20, required=True, label='Имя')
+    email = forms.EmailField(empty_value=True, required=True, label='Email')
+    entry = forms.CharField(max_length=2000, required=True, label='Текст', widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
