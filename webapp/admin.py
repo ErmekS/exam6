5f066b06fd1f3f9f8e8c09157c0c30c37a4ffd72
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-02 13:01:43 +0600

    создал шаблон базы данных

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Guestbook
+
+
+class GuestbookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'entry', 'created_time', 'updated_time', 'status']
+    list_display_links = ['entry']
+    list_filter = ['status']
+    search_fields = ['status']
+    fields = ['entry', 'status', 'created_time', 'updated_time']
+    readonly_fields = ['created_time', 'updated_time']
+
+
+admin.site.register(Guestbook, GuestbookAdmin)
