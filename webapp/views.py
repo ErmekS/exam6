123404a9b6506f74c29c2be8423eed61c052b637
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-02 14:40:16 +0600

    создали главную страницу, и просмотр всех активных записей

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+# from webapp.forms import SketchpadForm
+from webapp.models import Guestbook, STATUS_CHOICES
+
+
+def index_view(request):
+    guestbooks = Guestbook.objects.order_by("-updated_time")
+    context = {"guestbooks": guestbooks}
+    return render(request, "index.html", context)
+
